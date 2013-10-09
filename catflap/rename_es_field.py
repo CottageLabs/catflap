4b4409be8218b944dbc2bbdaf01affc2d9a61e5d
Emanuil Tolev <emanuil.tolev@gmail.com>
2013-10-09 07:35:13 +0100

    if destination is already present, merge source and destination values into a list; also add basic filtering support

--- catflap/rename_es_field.py
+++ catflap/rename_es_field.py
@@ -1,26 +1,36 @@
-'''
+"""
 Rename a field in all indexed documents in the acat index.
 Usage:
 python rename_es_field.py <source name> <destination name>
+    [elasticsearch query string]
 
 <source name> will be deleted after its contents are copied to
-<destination name>, for every document that is processed (all of them
-by default).
+<destination name>, for every document that is processed.
+
+You can optionally pass in [elasticsearch query string] in order to
+determine which documents should be processed. The value is whatever
+you would  pass to elasticsearch via the HTTP GET API in the "q"
+parameter:
+
+    http://localhost:9200/acat/journal/_search?q=[elasticsearch query string]
 
 Alternatively, from a python script or the python interpreter:
 from catflap import rename_es_field
 rename_es_field.rename_field(source_name, destination_name)
-'''
-import json
+
+or
+
+rename_es_field.rename_field(source_name, destination_name,
+    elasticsearch_query_string)
+"""
 
 import sys
-import requests
 from models import Journal
 
 
-def rename_field(src, dst):
+def rename_field(src, dst, q=None):
     # TODO do this using the scroll API http://www.elasticsearch.org/guide/reference/api/search/search-type/
-    everything = Journal.query(size=10000000)
+    everything = Journal.query(q=q, size=10000000)
 
     try:
         if everything['hits']['total'] <= 0:
@@ -32,11 +42,22 @@ def rename_field(src, dst):
 
     for record in everything['hits']['hits']:
         instance = Journal(**record['_source'])
-        instance[dst] = instance[src]
-        del instance[src]
+        # if both source and destination are present, merge them as
+        # a list
+        if src in instance and dst in instance:
+            instance[dst] = Journal.make_merge_list(
+                instance[dst], instance[src]
+            )
+        else:
+            instance[dst] = instance[src]
+        del instance[src]  # delete original field
         instance.save()
     Journal.refresh()
 
 
 if __name__ == '__main__':
-    rename_field(sys.argv[1], sys.argv[2])
+    if len(sys.argv) == 3:  # only source and destination of rename
+        rename_field(sys.argv[1], sys.argv[2])
+    elif len(sys.argv) == 4:  # also includes ES query string,
+                              # apply rename only to its results
+        rename_field(sys.argv[1], sys.argv[2], sys.argv[3])
